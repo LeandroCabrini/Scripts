f38784c86cc303f19f906fff2cefe79e8dbab906
Leandro Girotto Cabrini Junior <98980169+LeandroCabrini@users.noreply.github.com>
2024-09-11 19:16:44 -0300

    Create alonecrack.py

---
+++ alonecrack.py
@@ -0,0 +1,26 @@
+import crypt,sys
+
+#Verifica se foi passado wordlist como argumento
+if(len(sys.argv) < 2):
+        print("Modo de uso: python cracker.py /caminho/para/wordlist")
+        sys.exit(1)
+
+#Obtém caminho do arquivo wordlist
+wordlist = sys.argv[1]
+
+#Pergunta ao usuário qual o hash que ele deseja quebrar
+hash = input("HASH= ")
+
+#Pergunta o salt para o usuário
+salt = input("SALT = ")
+
+#Abre o arquivo em modo leitura
+with open(wordlist, 'r') as wd:
+        #intere sobre cada linha da wordlist
+        for linha in wd:
+                senha = linha.strip()
+                #Encripita senha com base no salt passado 
+                hashes = crypt.crypt(senha,salt)
+                if(hash == hashes):
+                        print("Senha encontrada: " + senha)
+                        break
